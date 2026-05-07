6f4d619846e5002fb76463a1bcbe20f36c22d4cd
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2026-05-07 11:57:03 +0530

    feat: create modern landing page and signin UI for freelance platform

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import{Signin} from './components/auth/signin/signin'
+import {LandingPage} from './components/landing-page/landing-page'
+
+export const routes: Routes = [
+    {
+        path:'',
+        component:LandingPage
+    },
+    {
+        path:'signin',
+        component:Signin
+    }
+];
---
+++ Frontend/src/app/components/landing-page/landing-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+// import {Header} from '../../Pages/header/header'
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [RouterLink],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.css',
+})
+export class LandingPage {}
